3ac39715d9628f9a7dac5a1be5f6749db010463c
Patrick Jenifer <pjuofa95@gmail.com>
2018-01-01 21:35:10 -0700

    data binding

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import { Employee } from '../models/employee.model';
 
 @Component({
   selector: 'home',
@@ -7,5 +8,24 @@ import {Component} from '@angular/core';
 })
 export class HomeComponent {
   languages = ['English' , 'Spanish', 'Other'];
+  model = new Employee ('Darla', 'Smith', true, 'w2', 'english');
+
+  firstNameToUpperCase(value: string) {
+    if (value.length > 0)
+      this.model.firstName = value.charAt(0).toUpperCase() + value.slice(1);
+      else
+      this.model.firstName = value;
+    
+  }
+
+  lastNameToUpperCase(value: string) {
+    if (value.length > 0)
+      this.model.lastName = value.charAt(0).toUpperCase() + value.slice(1);
+      else
+      this.model.lastName = value;
+    
+  }
+
+
 
 }
